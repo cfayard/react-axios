996ffc2eeb9bbc1e0b27651fb6f9b76defdf8d2b
Casey Fayard <fayardcasey@gmail.com>
2020-12-15 14:32:40 -0500

    axios delete

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -5,6 +5,7 @@ import axios from "axios";
 export default function PersonList(props) {
   const [persons, setPersons] = useState([]);
   const [name, setName] = useState("");
+  const [id, setId] = useState("");
 
   useEffect(() => {
     axios.get(`https://jsonplaceholder.typicode.com/users`).then((res) => {
@@ -15,6 +16,7 @@ export default function PersonList(props) {
 
   const handleChange = (event) => {
     setName({ name: event.target.value });
+    setId({ id: event.target.value });
   };
 
   const handleSubmit = (event) => {
@@ -25,6 +27,16 @@ export default function PersonList(props) {
     };
     axios
       .post(`https://jsonplaceholder.typicode.com/users`, { user })
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+      });
+  };
+
+  const handleDelete = (event) => {
+    event.preventDefault();
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/users/${id}`)
       .then((res) => {
         console.log(res);
         console.log(res.data);
@@ -44,6 +56,13 @@ export default function PersonList(props) {
           <input type="text" name="name" onChange={handleChange} />
         </label>
         <button type="submit">Add</button>
+      </form>
+      <form onSubmit={handleDelete}>
+        <label>
+          Person ID:
+          <input type="text" name="id" onChange={handleChange} />
+        </label>
+        <button type="submit">Delete</button>
       </form>
     </div>
   );
